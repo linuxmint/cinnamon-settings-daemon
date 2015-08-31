a17aea91d2a360d7428892bd585dba555ebd5081
leigh123linux <leigh123linux@googlemail.com>
2015-08-31 10:31:12 +0100

    orientation: Fix screen lock not getting applied

--- plugins/orientation/csd-orientation-manager.c
+++ plugins/orientation/csd-orientation-manager.c
@@ -478,9 +478,9 @@ csd_orientation_manager_idle_cb (CsdOrientationManager *manager)
         cinnamon_settings_profile_start (NULL);
 
         manager->priv->settings = g_settings_new (CONF_SCHEMA);
-        manager->priv->orientation_lock = g_settings_get_boolean (manager->priv->settings, ORIENTATION_LOCK_KEY);
         g_signal_connect (G_OBJECT (manager->priv->settings), "changed::orientation-lock",
                           G_CALLBACK (orientation_lock_changed_cb), manager);
+        manager->priv->orientation_lock = g_settings_get_boolean (manager->priv->settings, ORIENTATION_LOCK_KEY);
 
         manager->priv->client = g_udev_client_new (subsystems);
         dev = get_accelerometer (manager->priv->client);
