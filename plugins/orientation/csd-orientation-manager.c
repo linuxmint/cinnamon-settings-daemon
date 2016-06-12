3a3deca6c923a537e3b4bc6b6d8287a82cd6006a
Jakub Adam <jakub.adam@ktknet.cz>
2016-06-12 17:08:30 +0200

    orientation: Fix screen lock not getting applied
    Read value after connecting to changed:: signal, as it's a requirement
    for the signal to be emitted at all.
    
    See https://bugzilla.gnome.org/show_bug.cgi?id=750257 for details.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=753155
    
    Port of 55410811 GNOME settings daemon commit.

--- plugins/orientation/csd-orientation-manager.c
+++ plugins/orientation/csd-orientation-manager.c
@@ -437,9 +437,9 @@ csd_orientation_manager_start (CsdOrientationManager *manager,
         cinnamon_settings_profile_start (NULL);
 
         manager->priv->settings = g_settings_new (CONF_SCHEMA);
-        manager->priv->orientation_lock = g_settings_get_boolean (manager->priv->settings, ORIENTATION_LOCK_KEY);
         g_signal_connect (G_OBJECT (manager->priv->settings), "changed::" ORIENTATION_LOCK_KEY,
                           G_CALLBACK (orientation_lock_changed_cb), manager);
+        manager->priv->orientation_lock = g_settings_get_boolean (manager->priv->settings, ORIENTATION_LOCK_KEY);
 
         g_dbus_proxy_new_for_bus (G_BUS_TYPE_SESSION,
                                   G_DBUS_PROXY_FLAGS_NONE,
