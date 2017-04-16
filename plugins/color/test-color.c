7b7b39dd855a4e5f7e0e6094c53d0ec6c01244f1
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-04-16 15:48:30 +0200

    color: Add test application
    https://bugzilla.gnome.org/show_bug.cgi?id=741688

---
+++ plugins/color/test-color.c
@@ -0,0 +1,7 @@
+#define NEW csd_color_manager_new
+#define START csd_color_manager_start
+#define STOP csd_color_manager_stop
+#define MANAGER CsdColorManager
+#include "csd-color-manager.h"
+
+#include "test-plugin.h"
