5716d056eac8d55516a18be5cd59a0a142351339
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2017-04-16 16:12:08 +0200

    dummy: Add test application
    https://bugzilla.gnome.org/show_bug.cgi?id=772370

---
+++ plugins/dummy/test-dummy.c
@@ -0,0 +1,7 @@
+#define NEW csd_dummy_manager_new
+#define START csd_dummy_manager_start
+#define STOP csd_dummy_manager_stop
+#define MANAGER CsdDummyManager
+#include "csd-dummy-manager.h"
+
+#include "test-plugin.h"
