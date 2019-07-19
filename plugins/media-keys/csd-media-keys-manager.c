0c0ff150fde1ddc658a2fb7ee10dd34523509da2
Corbin <ravetcofx@gmail.com>
2019-07-19 00:40:11 -0600

    Fix Shutdown immediately not working
    New command to power off thanks to systemd that actually works. Not sure when that old command stopped working.
    Better Option

--- plugins/media-keys/csd-media-keys-manager.c
+++ plugins/media-keys/csd-media-keys-manager.c
@@ -1483,8 +1483,7 @@ do_config_power_action (CsdMediaKeysManager *manager,
                 cinnamon_session_shutdown (manager);
                 break;
         case CSD_POWER_ACTION_SHUTDOWN:
-                //FIXME: A wee bit cheating here...
-                execute (manager, "dbus-send --dest=org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.RequestShutdown", FALSE);
+                csd_power_poweroff (manager->priv->use_logind);
                 break;
         case CSD_POWER_ACTION_HIBERNATE:
                 csd_power_hibernate (manager->priv->use_logind);
